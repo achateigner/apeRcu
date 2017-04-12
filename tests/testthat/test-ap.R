5ab6cf44f3aa6bb99a32c31c42f847265efc2dc4
Aurelien Chateigner <clerua@gmail.com>
2017-04-12 10:28:21 +0200

    Started to ad testthat tests

--- tests/testthat/test-ap.R
+++ tests/testthat/test-ap.R
@@ -3,21 +3,27 @@ context("ap")
 ## TODO: Rename context
 ## TODO: Add more tests
 # Creation of objects
-# ap(v)
-# ap(m)
-# ap(df)
-# ap(li)
-# ap(a)
-# ap(a2)
-# ap(a3)
-# sm <- matrix(1:4, 2, 2)
-# sDf <- as.data.frame(sm)
-# library(pls)
-# data("gasoline")
-# ap(gasoline)
-#
+v <- c(1:20)
+names(v) <- letters[1:20]
+m <- matrix(1:100, 10, 10)
+dimnames(m) <- list(letters[1:10], letters[1:10])
+df <- as.data.frame(m)
+li <- lapply(1:10, function(x) {u <- matrix((1:100)*x, 10,10); dimnames(u) <- list(letters[1:10], letters[1:10])
+return(u)})
+names(li) <- letters[1:10]
+a <- array(c(1:1000), c(10,10,10))
+dimnames(a) <- list(letters[1:10], letters[1:10], letters[1:10])
+a2 <- array(1:10000, c(10,10,10,10))
+dimnames(a2) <- list(letters[1:10], letters[1:10], letters[1:10], letters[1:10])
+a3 <- array(1:100000, c(10,10,10,10,10))
+dimnames(a3) <- list(letters[1:10], letters[1:10], letters[1:10], letters[1:10], letters[1:10])
+sm <- matrix(1:4, 2, 2)
+sDf <- as.data.frame(sm)
+library(pls)
+data("gasoline")
 
 
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+
+test_that("works on vector", {
+  expect_equal(ap(v)$apercu, v[1:5])
 })
